75aa0162c20deebcf5baa31d9326efd40095437e
Larsoooo <78809483+Larsoooo@users.noreply.github.com>
2023-01-17 11:37:23 +0100

    Update main.cpp

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -5,13 +5,9 @@
 #define LED 26
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED, OUTPUT);
+  
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  delay(200);
-  digitalWrite(LED, LOW);
-  delay(200);
+  
 }
