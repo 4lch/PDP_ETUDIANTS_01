a3e63cc313cd7d03925ef631b791eb8c3d7f7569
Larsoooo <78809483+Larsoooo@users.noreply.github.com>
2023-01-24 17:40:49 +0100

    etape_2

--- PDP_BREADBOARD/src/main.cpp
+++ PDP_BREADBOARD/src/main.cpp
@@ -1,13 +1,33 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 // Define the pins that we will use
 #define SENSOR 33
 #define LED 26
 
+
+DHT dht(33, DHT11);
+
 void setup() {
-  
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+
+   dht.begin();
+   Serial.begin(9600);
 }
 
 void loop() {
-  
+ 
+   float h = dht.readHumidity();
+   float t = dht.readTemperature();
+  // Effectuer une mesure toutes les 5s
+
+  // Afficher l'humidité relative avec un peu de formatage
+   Serial.print("Humidity: ");
+   Serial.println(h);
+  // Afficher la température avec un peu de formatage
+   Serial.print("Temperature: ");
+   Serial.println(t);
 }
